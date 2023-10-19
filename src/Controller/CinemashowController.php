e3a78454e25b31fb0e69c8f42f7a150614976d49
flo <floo33@hotmail.fr>
2023-10-19 12:32:56 +0200

    Adding fonctionnal reservation for cinema shows

--- src/Controller/CinemashowController.php
+++ src/Controller/CinemashowController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Model\ItemManager;
 use App\Model\CinemashowManager;
+use App\Model\BookingManager;
 use App\Model\UserManager;
 use Twig\Error\LoaderError;
 use Twig\Error\RuntimeError;
@@ -13,8 +14,24 @@ class CinemashowController extends AbstractController
 {
     public function show(int $id): string
     {
-        $cinemashowManager = new CinemashowManager();
-        $cinemashow = $cinemashowManager->selectJoinById($id);
-        return $this->twig->render('Item/movie.html.twig', ['cinemashow' => $cinemashow]);
+        $userData = $_SESSION['user_id'] ?? [];
+        $booking = array();
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            if (isset($_POST['nbr_seat'])) {
+                $cinemashowManager = new CinemashowManager();
+                $bookingManager = new BookingManager();
+                $cinemashow = $cinemashowManager->selectJoinById($id);
+                $booking['nameBooking'] = 'Florian';
+                $booking['numberPlace'] = intval($_POST['nbr_seat']);
+                $booking['id_user'] = 1;
+                $booking['id_CinemaShow'] = intval($_POST['csId']);
+                $bookingManager->insert($booking);
+            }
+        } else {
+            $cinemashowManager = new CinemashowManager();
+            $cinemashow = $cinemashowManager->selectJoinById($id);
+        }
+        return $this->twig->render('Item/movie.html.twig', ['cinemashow' => $cinemashow, 'userData' => $userData]);
     }
+
 }
