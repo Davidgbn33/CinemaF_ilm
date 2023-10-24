d9ee48dc92e3c8ee69b51fedb0cc6caa65d64a62
gibonDavid <gibon.david@live.fr>
2023-10-24 10:52:59 +0200

    fix all bug

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,31 +2,35 @@
 
 namespace App\Controller;
 
-use App\Model\MovieManager;
 use App\Model\UserManager;
 
-/**
- * Class AdminController
- * @package App\Controller
- * @property UserManager
- * @property MovieManager
- */
 class AdminController extends AbstractController
 {
     /**
-     *visualisation de la page  admin
+     * visualisation des utilisateur
      * @return string
      */
     public function index(): string
     {
         $userManager = new userManager();
-        $movieManager = new MovieManager();
-        return $this->extracted($userManager, $movieManager);
-
+        $users = $userManager->selectAll();
+        $userData = $_SESSION['user_id'] ?? [];
+        if (isset($_SESSION['user_id'])) {
+            $userManager = new UserManager();
+            $user = $userManager->selectOneById($_SESSION['user_id']);
+        } else {
+            $user = [];
+        }
+        return $this->twig->render(
+            'Admin/admin.html.twig',
+            ['users' => $users,
+                'userData' => $userData,
+                'user' => $user]
+        );
     }
 
     /**
-     * function pour supprimer un utilisateur
+     * suppression d'un utilisateur
      * @param int $id
      * @return string
      */
@@ -34,33 +38,14 @@ class AdminController extends AbstractController
     {
         $userManager = new UserManager();
         $userManager->delete($id);
-        return $this->extracted($userManager);
-    }
-
-    /**
-     * function pour modifier un utilisateur
-     * @param UserManager $userManager
-     * @param MovieManager $movieManager
-     * @return string
-     */
-    public function extracted(UserManager $userManager, MovieManager $movieManager): string
-    {
-        $movies = $movieManager->selectAll();
         $users = $userManager->selectAll();
         $userData = $_SESSION['user_id'] ?? [];
-        if (isset($_SESSION['user_id']) && $_SESSION['user']['role'] === "admin") {
+        if (isset($_SESSION['user_id'])) {
             $userManager = new UserManager();
             $user = $userManager->selectOneById($_SESSION['user_id']);
-            return $this->twig->render(
-                'Admin/admin.html.twig',
-                ['users' => $users,
-                    'userData' => $userData,
-                    'user' => $user,
-                    'movies' => $movies,]
-            );
         } else {
             $user = [];
-            return $this->twig->render('includes/404.html.twig',);
         }
+        return $this->twig->render('Admin/admin.html.twig', ['users' => $users, 'userData' => $userData, 'user' => $user]);
     }
 }
